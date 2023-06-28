fd847456e4d671a5db6cb895657e9a1ea9af57a9
alanhesketh <62773925+alanhesketh@users.noreply.github.com>
2023-06-27 18:55:55 -0700

    Move final exam project file

--- final-exam/final-exam/src/SearchBar.tsx
+++
@@ -1,7 +0,0 @@
-export default function SearchBar ({searchText, setSearchText}) {
-    return (
-        <>
-            <input type='text' id="new" placeholder="Search the menu" value={searchText} onChange={e => {setSearchText(e.target.value);}}/>
-        </>
-    )
-}
